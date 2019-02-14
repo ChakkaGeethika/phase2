42ddb0e2dda297f4a1996bb250965650cce6e2ef
unknown <geethika.ch.71@gmail.com>
2019-02-14 16:54:40 +0530

    commit

---
+++ store.js
@@ -0,0 +1,52 @@
+console.log("hello Geeth");
+function submit() {
+  var carreer=document.getElementById('carreer').value;
+  var name=document.getElementById('name').value;
+  var role=document.getElementById('role').value;
+  var phonenumber=document.getElementById('phonenumber').value;
+  var mailid=document.getElementById('mailid').value;
+  var degree=document.getElementById('degree').value;
+  var dcollege=document.getElementById('dcollege').value;
+  var branch=document.getElementById('branch').value;
+  var dmarks=document.getElementById('dmarks').value;
+  var idegree=document.getElementById('idegree').value;
+  var icollege=document.getElementById('icollege').value;
+  var ibranch=document.getElementById('ibranch').value;
+  var imarks=document.getElementById('imarks').value;
+  var board=document.getElementById('board').value;
+  var school=document.getElementById('school').value;
+  var medium=document.getElementById('medium').value;
+  var smarks=document.getElementById('smarks').value;
+  var skills=document.getElementById('skills').value;
+
+  var indexedDB=window.indexedDB||window.mozIndexedDB||window.wevkitIndexedDB||window.msIndexedDB;
+
+  //terinary operator
+  indexedDB?console.log("success"):console.log("browser Not Supported");
+
+  var request=indexedDB.open("DBMS",1);
+  var result;
+  var store;
+  console.log(request);
+  //upgradeneeded
+  request.onupgradeneeded=function(e) {
+    result=e.target.result;
+    result.createObjectStore("resume",{keypath:'id',autoIncrement:true});
+  }
+  //success
+  request.onsuccess=function(e) {
+    console.log("reached successfully");
+    result=e.target.result;
+    var tx=result.transaction("resume","readwrite");
+    store=tx.objectStore("resume");
+    store.put(
+      {
+      co:carreer
+      }
+    );
+  }
+  //error
+  request.onerror=function(e) {
+    console.log("error"+e);
+  }
+ }
